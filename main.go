2c10ef008290c28565acd238f857d956d9591f30
victor-s-santos <victor.s.silva@unesp.br>
2020-02-03 10:55:57 -0300

    generateblock function was created

--- main.go
+++ main.go
@@ -33,3 +33,16 @@ func calculateHash(block Block) string {
 	hashed := h.Sum(nil)
 	return hex.EncodeToString(hashed)
 }
+
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+	return newBlock, nil
+}
