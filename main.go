2ed500634387bfbac38cefa869677902b0da2e56
victor-s-santos <victor.s.silva@unesp.br>
2020-02-03 10:47:11 -0300

    main.go file created with all imports

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
+)
