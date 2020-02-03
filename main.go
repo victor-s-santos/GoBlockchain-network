14a5d89fe14332e984793d3391e73c37e83ea8e6
victor-s-santos <victor.s.silva@unesp.br>
2020-02-03 10:52:45 -0300

    hash function was created

--- main.go
+++ main.go
@@ -25,3 +25,11 @@ type Block struct {
 }
 
 var Blockchain []Block
+
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
